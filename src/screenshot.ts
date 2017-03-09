c17f4c60879f63b0db595a5f0d271d3a908aee39
David Hewitt <david.hewitt@indigotree.co.uk>
2017-03-09 14:38:22 +0000

    fixing dependencies

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -4,7 +4,7 @@ import phantomScreenshot from './phantom-shell'
 
 function screenshot(chunkFilepath: string, domain: string, environment: string): Promise<string[]> {
 	return hasReadAccess(chunkFilepath)
-		.then((filepath) => {
+		.then((filepath: string) => {
 			return phantomScreenshot(filepath, domain, environment)
 		})
 		.catch((error: string) => {
