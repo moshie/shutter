d7c47dc24d83f4e10434c110a6c2a1ccc0b1855b
David Hewitt <david.hewitt@indigotree.co.uk>
2017-03-31 16:15:12 +0100

    added doc blocks and changed return type

--- src/crawl/crawler.ts
+++ src/crawl/crawler.ts
@@ -7,19 +7,57 @@ import {environmentsInterface} from '../cli/environments-interface'
 
 class Crawler {
 
+    /**
+     * Screenshot environments
+     * @type {environmentsInterface}
+     */
     environments: environmentsInterface
+
+    /**
+     * Site spider
+     * @type {Spider}
+     */
     spider: Spider
+
+    /**
+     * Request user agent
+     * @type {string}
+     */
     userAgent: string
+
+    /**
+     * Crawled domain
+     * @type {string}
+     */
     domain: string
+
+    /**
+     * Screenshot file paths
+     * @type {string[]}
+     */
     paths: string[] = []
+
+    /**
+     * Links already run through the crawler
+     * @type {string[]}
+     */
     checked: string[] = []
 
+    /**
+     * Crawler constructor
+     * 
+     * @param {environmentsInterface} environments
+     */
     constructor(environments: environmentsInterface) { 
         this.environments = environments
         this.domain = environments[Object.keys(environments)[0]]
         this.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     }
 
+    /**
+     * Crawl 
+     * @return {Promise<any>} [description]
+     */
     crawl(): Promise<any> {
         return new Promise((resolve, reject) => {
             this.spider = new Spider({
@@ -33,7 +71,7 @@ class Crawler {
         })
     }
 
-    pageHandler(doc: Document): void {
+    protected pageHandler(doc: Document): void {
         var self = this;
         doc.$('a[href]').each(function (i: number, elem: any) {
             let href: string = doc.$(this).attr('href')
@@ -71,7 +109,7 @@ class Crawler {
         })
     }
 
-    error(error: Error, url: string) {
+    protected error(error: Error, url: string) {
 
     }
 
